0e12174fafe504ad786456c16442a651d8196fc2
Julien Valleau <julien.valleau@gmail.com>
2021-10-15 16:16:59 +0200

    Modifications majeurs

--- mgfactory/static/js/script.js
+++ mgfactory/static/js/script.js
@@ -13,3 +13,19 @@ function toggleFormFilter() {
 
 buttonFilter.onclick = toggleFormFilter; 
 buttonFilterClose.onclick = toggleFormFilter;
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('hour').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // Ajout un 0 devant le nombre si < 10
+  return i;
+}
